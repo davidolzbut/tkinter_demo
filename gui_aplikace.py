db26dc5c94ccc95ea67c9ebda3069372bde88a68
davidolzbut <david.olzbut@sps-prosek.cz>
2022-03-01 15:08:06 +0100

    dd

--- gui_aplikace.py
+++ gui_aplikace.py
@@ -1,4 +1,13 @@
 import tkinter as tk
+from tkinter import messagebox
+
+
+def get_user_input():
+    user_input = entry1.get()
+    messagebox.showinfo("Pozdrav", "Ahoj " + user_input + "!")
+    entry1.delete(0, tk.END)
+    entry1.insert(0, "!")
+
 
 window = tk.Tk()
 window.geometry("200x200")
@@ -17,6 +26,25 @@ label1 = tk.Label(
     background="black",
     foreground="white"
 )
-label1.pack()
+
+label1.grid(row=0, column=1, columnspan=2, sticky="W")
+
+
+entry1 = tk.Entry(
+    width=20,
+)
+entry1.grid(row=1, column=1)
+
+button1 = tk.Button(
+    text="Klik!",
+    bg="green",
+    fg="purple",
+    width=20,
+    height=5,
+    command=get_user_input
+)
+
+button1.grid(row=1, column=2)
+
 
 window.mainloop()
